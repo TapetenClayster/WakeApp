616b918819fccf42f30fefc1f3151756c56da440
Yupiel <stefan_mitrovic@outlook.com>
2022-06-17 05:47:00 +0200

    fix issues 1 and 2

--- src/main/java/org/ssc/model/Location.java
+++ src/main/java/org/ssc/model/Location.java
@@ -23,7 +23,7 @@ public class Location {
         this.bvgId = null;
     }
 
-    public Location(String name, String longitude, String latitude, String bvgId){
+    public Location(String name, String longitude, String latitude, String bvgId) {
         this.name = name;
         this.street = null;
         this.housenumber = null;
@@ -35,19 +35,31 @@ public class Location {
         this.bvgId = bvgId;
     }
 
+    public Location(String name, String longitude, String latitude) {
+        this.name = name;
+        this.street = null;
+        this.housenumber = null;
+        this.postalcode = null;
+        this.region = null;
+        this.country = null;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.bvgId = null;
+    }
+
     public String toString() {
         String output = "";
 
-        if (!this.name.isEmpty()){
+        if (!this.name.isEmpty()) {
             output += this.name + "\n";
         }
-        if(this.street != null && !this.street.isEmpty() && !this.street.equals(this.name)){
-            if(this.housenumber != null && this.housenumber.isEmpty())
+        if (this.street != null && !this.street.isEmpty() && !this.street.equals(this.name)) {
+            if (this.housenumber != null && this.housenumber.isEmpty())
                 output += this.street + "\n";
             else
                 output += String.format("%s, %s\n", this.street, this.housenumber);
         }
-        if(this.postalcode != null && !this.postalcode.isEmpty()){
+        if (this.postalcode != null && !this.postalcode.isEmpty()) {
             output += String.format("%s, %s\n", this.postalcode, this.region);
         } else {
             output += String.format("%s\n", this.region);
